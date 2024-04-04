a4fd6fd780cef62bbea0f6787d967ea20aed90d9
UncleVasya <unclevasyaai@gmail.com>
2024-04-04 21:39:19 +0300

    added translation wrapper for api endpoints

--- fastapi_localization/route.py
+++ fastapi_localization/route.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from typing import Callable
 
 from fastapi import (
@@ -11,8 +12,25 @@ from fastapi_localization.response import TranslateJsonResponse
 
 class LocalizationRoute(APIRoute):
     """
-    Route that localization response
+    Route that localizes response
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        def translate(func):
+            """ Endpoint wrapper that does result translation """
+
+            @wraps(func)
+            async def wrapper(*args, **kwargs) -> TranslateJsonResponse:
+                response_data = await func(*args, **kwargs)
+                return TranslateJsonResponse(content=response_data.model_dump())
+
+            return wrapper
+
+        self.endpoint = translate(self.endpoint)
+
+
     def get_route_handler(self) -> Callable:
         original_route_handler = super().get_route_handler()
 
